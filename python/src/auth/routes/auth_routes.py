9915ec9890356ac7b3f64c7c3aae7cd6770891e5
Jia Qing Tee <76116069+jiaqing-tee@users.noreply.github.com>
2024-06-10 14:36:58 +0800

    Minor clean up

--- python/src/auth/routes/auth_routes.py
+++ python/src/auth/routes/auth_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from controllers import auth_controller
 
+
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 auth_bp.route('/login', methods=['POST'])(auth_controller.login)
 auth_bp.route('/validate', methods=['POST'])(auth_controller.validate)
